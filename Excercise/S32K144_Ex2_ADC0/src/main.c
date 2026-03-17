e97cb2d4d789ee42962eabe7b474b11b40d5f862
0xFish <144463079+solarsvxc@users.noreply.github.com>
2026-03-17 21:04:40 +0700

    +Update: ex2 release

--- Excercise/S32K144_Ex2_ADC0/src/main.c
+++ Excercise/S32K144_Ex2_ADC0/src/main.c
@@ -1,99 +1,84 @@
-/**
- * @file main.c
- * @author duongnn (you@domain.com)
- * @brief
- * @version 0.1
- * @date 2025-09-26
- *
- * @copyright Copyright (c) 2025
- *
- */
-#include "S32K144.h"
+#include <stdint.h>
+#include <S32K144.h>
+// #define BTN1_PIN        12U
+// #define BTN2_PIN        13U
 
-#define POTENTIOMETER_C 14
-#define BLUE_D           0
-#define GREEN_D    16
-#define RED_D      15
+#define BLUE_LED_PIN    0U
+#define GREEN_LED_PIN   16U
+#define POT_A           6U
 
-uint32_t adcResultInMv_pot = 0;
-uint32_t adcResultInMv_Vrefsh = 0;
-
-
-uint8_t adc_complete(void)  {
-  return ((IP_ADC0->SC1[0] & ADC_SC1_COCO_MASK)>>ADC_SC1_COCO_SHIFT); /* Wait for completion */
-}
-uint32_t read_adc_chx(void)  {
-    uint16_t adc_result=0;
-    adc_result=IP_ADC0->R[0];      /* For SW trigger mode, R[0] is used */
-    return  (uint32_t) ((5000*adc_result)/0xFFF); /* Convert result to mv for 0-5V range */
-}
-/**
- * @brief
- *
- * @return
- */
 int main(void)
 {
-    /*<======================ADC0-Init===========================>*/
-    /* Disable clock */
-    IP_PCC->PCCn[PCC_ADC0_INDEX] &=~ PCC_PCCn_CGC_MASK;
-    /* Enable bus clock in ADC 	*/
+//    uint32_t vref = 0;
+    uint32_t value = 0;
+
+    IP_PCC->PCCn[PCC_PORTD_INDEX] &=~ PCC_PCCn_CGC_MASK;
+    IP_PCC->PCCn[PCC_PORTA_INDEX] &=~ PCC_PCCn_CGC_MASK;
+    IP_PCC->PCCn[PCC_ADC0_INDEX]  &=~ PCC_PCCn_CGC_MASK;
+
+    IP_PCC->PCCn[PCC_PORTD_INDEX] |= PCC_PCCn_CGC_MASK;
+    IP_PCC->PCCn[PCC_PORTA_INDEX] |= PCC_PCCn_CGC_MASK;
+
+    IP_PORTD->PCR[BLUE_LED_PIN]   |= PORT_PCR_MUX(1);
+    IP_PORTD->PCR[GREEN_LED_PIN]  |= PORT_PCR_MUX(1);
+
+    IP_PORTA->PCR[POT_A] &=~ PORT_PCR_MUX_MASK;
+
+    IP_PTD->PDDR |= (1 << BLUE_LED_PIN) | (1 << GREEN_LED_PIN);
+    IP_PTD->PCOR = (1 << BLUE_LED_PIN);
+	IP_PTD->PCOR = (1 << GREEN_LED_PIN);
+
+//    IP_SCG->FIRCCSR  &=~ SCG_FIRCCSR_FIRCEN_MASK;
+
+    IP_SCG->SOSCCFG |= SCG_SOSCCFG_EREFS(1); /* external osc */
+    IP_SCG->SOSCCFG |= SCG_SOSCCFG_RANGE(0b10); /* Range error: auto set High range  */
+
+    IP_SCG->SOSCCSR |= SCG_SOSCCSR_SOSCEN(1);
+    while (!(IP_SCG->SOSCCSR & SCG_SOSCCSR_SOSCVLD_MASK)){}
+    
+    IP_SCG->SOSCDIV &=~ SCG_SOSCDIV_SOSCDIV1_MASK;
+    IP_SCG->SOSCDIV |= SCG_SOSCDIV_SOSCDIV2(1);
+    
+    IP_SCG->RCCR    &=~ SCG_RCCR_SCS_MASK;
+    IP_SCG->RCCR    |= SCG_RCCR_SCS(1); /* System OSC (SOSC_CLK) */  
+
+    // while (((IP_SCG->CSR & SCG_CSR_SCS_MASK) >> SCG_CSR_SCS_SHIFT) != 1);
+
+    IP_PCC->PCCn[PCC_ADC0_INDEX] |= PCC_PCCn_PCS(1);
     IP_PCC->PCCn[PCC_ADC0_INDEX] |= PCC_PCCn_CGC_MASK;
-    /* ADCH=1F: Module is disabled for conversions	*/
-    IP_ADC0->SC1[0] |= ADC_SC1_ADCH_MASK;
-    /* CFG1 MODE = 00b -> 10 bit conversion */
-    /* ADIV = 00b -> input by rate clock */
-    IP_ADC0->CFG1   = ADC_CFG1_ADIV(0) | ADC_CFG1_MODE(0);
-    /* AIEN disable */
-    IP_ADC0->SC1[0] |= ADC_SC1_AIEN(0);
-    /* SMPLTS default */
-    IP_ADC0->CFG2   = ADC_CFG2_SMPLTS(0xC);
-    /* */
-    IP_ADC0->SC2    = 0x00000000;
-    IP_ADC0->SC3    = 0x00000000;
+    
+    IP_ADC0->CFG1 &=~ ADC_CFG1_ADICLK_MASK;
+    IP_ADC0->CFG1 &=~ ADC_CFG1_ADIV_MASK;
+    IP_ADC0->CFG1 |=  ADC_CFG1_MODE(1);
 
-    /*<======================GPIO-PORT Init===========================>*/
-    /*<! Disable CGC on PORTC */
-    IP_PCC->PCCn[PCC_PORTC_INDEX] &= ~PCC_PCCn_CGC_MASK;
-    /*<! Enable CGC on PORTC */
-    IP_PCC->PCCn[PCC_PORTC_INDEX]  |= PCC_PCCn_CGC_MASK;
-    /*<! Disable CGC on PORTD */
-    IP_PCC->PCCn[PCC_PORTD_INDEX] &= ~PCC_PCCn_CGC_MASK;
-    /*<! Disable CGC on PORTD */
-    IP_PCC->PCCn[PCC_PORTD_INDEX]  |= PCC_PCCn_CGC_MASK;
+    IP_ADC0->SC2  &=~ ADC_SC2_REFSEL_MASK; 
+    IP_ADC0->SC2  &=~ ADC_SC2_ADTRG_MASK;
 
-    IP_PORTC->PCR[POTENTIOMETER_C]    = PORT_PCR_MUX(0);
-
-    IP_PORTD->PCR[GREEN_D]            = PORT_PCR_MUX(1);
-    IP_PORTD->PCR[BLUE_D]             = PORT_PCR_MUX(1);
-    IP_PORTD->PCR[RED_D]              = PORT_PCR_MUX(1);
-
-    IP_PTD->PDDR                          |= (1U<<BLUE_D)  | (1U<<GREEN_D) | (1U<<RED_D);
-    IP_PTD->PSOR                        |= (1U<<GREEN_D) | (1U<<BLUE_D)  | (1U<<RED_D);
-
+    
+    IP_ADC0->CFG2    = ADC_CFG2_SMPLTS(20);
+    
     while (1)
     {
-        /* Initiate new conversion by writing to ADC0_SC1A(ADCH) */
-        /* ADCH = 12: External channel 12 as input */
-        IP_ADC0->SC1[0] |= ADC_SC1_ADCH(0x11);
-        /* Check SC1[0] value at sequence 0 compare with ADC_SC1_COCO_MASK <value at bit number 8 of register SC1>
-        *  Then shift right value compared */
-//        while (adc_complete()==0){};
+        IP_ADC0->SC1[0] &= ~ADC_SC1_ADCH_MASK;
+        IP_ADC0->SC1[0] |= ADC_SC1_ADCH(12);
 
-        adcResultInMv_pot = IP_ADC0->R[0];
-
-        if ( 0 <  adcResultInMv_pot && adcResultInMv_pot <= 341 )
+        while((IP_ADC0->SC1[0] & ADC_SC1_COCO_MASK)>>ADC_SC1_COCO_SHIFT){}
+        
+        value = IP_ADC0->R[0];
+        if (1000 <= value && value <= 2000)
         {
-            IP_PTD->PTOR   = (1U<<RED_D);
-        } else if (341 < adcResultInMv_pot && adcResultInMv_pot <= 682)
-        {
-            IP_PTD->PTOR   = (1U<<BLUE_D);
-        } else if ( 682 < adcResultInMv_pot && adcResultInMv_pot <= 1023)
-        {
-            IP_PTD->PTOR   = (1U<<GREEN_D);
+            IP_PTD->PSOR = (1<<BLUE_LED_PIN);
+        } else {
+            
+            IP_PTD->PCOR = (1<<BLUE_LED_PIN);
         }
-        /* DELAY function */
-        for (uint8_t i = 0; i < 100; i++){}
-    }
-}
 
+        if (2000 <= value)
+        {
+            IP_PTD ->PSOR = (1<<GREEN_LED_PIN);
+        } else {
+            IP_PTD ->PCOR = (1<<GREEN_LED_PIN);
+        }
+    }
+    return 0;
+}
