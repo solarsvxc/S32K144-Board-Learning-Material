485e79683d34bcdd66dcc8f6397fd6e78c31fd41
0xFish <144463079+solarsvxc@users.noreply.github.com>
2026-03-17 19:31:49 +0700

    + Remove GPIO

--- Driver_GPIO/Debug_FLASH/src/App/main.d
+++
@@ -1,7 +0,0 @@
-src/App/main.o: ../src/App/main.c \
- B:/s32workspace/test_asg1/include/S32K144.h \
- B:/s32workspace/test_asg1/src/Driver/include/driver_gpio.h \
- B:/s32workspace/test_asg1/src/Driver/include/driver_common.h
-B:/s32workspace/test_asg1/include/S32K144.h:
-B:/s32workspace/test_asg1/src/Driver/include/driver_gpio.h:
-B:/s32workspace/test_asg1/src/Driver/include/driver_common.h:
